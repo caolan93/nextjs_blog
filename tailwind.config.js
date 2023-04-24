85f412831b7899950bcd61b939753fa50df2b45f
caolan <caolan@digigrow.ie>
2023-04-24 18:26:04 -0400

    updating tailwindcss file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,26 +7,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "wh-10": "#f4f4f4",
+        "wh-10": "#F4F4F4",
         "wh-50": "#FBFBFB",
-        "wh-300": "#C9C9C9",
+        "wh-100": "#C9C9C9",
+        "wh-300": "#939393",
         "wh-500": "#595959",
-        "wh-700": "#0f0f0f",
-        "wh-900": "#090909",
+        "wh-900": "#0F0F0F",
         "accent-red": "#EA9648",
         "accent-orange": "#F6CF68",
         "accent-green": "#C2E9B4",
       },
+      backgroundImage: (theme) => ({
+        "gradient-gradual":
+          "linear-gradient(180deg, rgba(116, 116, 116, 0) 66.15%, #000000 100%)",
+      }),
     },
-    backgroundImage: (theme) => ({
-      "gradient-gradual":
-        "linear-gradient(180deg, rgba(116,116,116,0) 66.15%, #000000 100%",
-    }),
-  },
-  screens: {
-    xs: "480px",
-    sm: "768px",
-    md: "1060px",
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
