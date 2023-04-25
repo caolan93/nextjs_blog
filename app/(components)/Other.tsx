4a147078eac01e0f4219607741f524979de9bbff
caolan <caolan@digigrow.ie>
2023-04-25 14:34:04 -0400

    Other application done

--- app/(components)/Other.tsx
+++ app/(components)/Other.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Card from "./Card";
 
 type Props = {};
 
 const Other = (props: Props) => {
-  return <div>Other</div>;
+  return (
+    <section>
+      <hr className="border-1" />
+      <div className="flex items-center gap-3 my-8">
+        <p className="font-bold text-2xl my-8 sm:text-3xl">
+          Other Trending Posts
+        </p>
+      </div>
+      <div className="sm:grid grid-cols-2 gap-16">
+        <Card imageHeight="h-80" className="bg-wh-500 mt-5 sm:mt-0" />
+        <Card imageHeight="h-80" className="bg-wh-500 mt-5 sm:mt-0" />
+        <Card imageHeight="h-80" className="bg-wh-500 mt-5 sm:mt-0" />
+        <Card imageHeight="h-80" className="bg-wh-500 mt-5 sm:mt-0" />
+      </div>
+    </section>
+  );
 };
 
 export default Other;
